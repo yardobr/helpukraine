ef008aad616537465fc5078c2b5936fc3492fc03
yardobr <yar.dobroskok@gmail.com>
2022-04-07 19:48:14 +0300

    Updated template link

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 (function() {
     var node = document.createElement('iframe');
-    node.src = 'https://storage.googleapis.com/helpua/footer.html';
+    node.src = 'https://cdn.jsdelivr.net/gh/yardobr/helpukraine@0.0.1/src/templates/footer.html';
     node.width = '100%';
     node.height = '48px';
-    node.style = 'position:fixed; bottom: 0; height: 48px; border: none;'
+    node.style = 'position:fixed; bottom: 0; height: 48px; border: none;z-index: 10000;'
     document.body.appendChild(node);
 })();
