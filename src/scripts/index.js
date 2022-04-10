86c74eb338a2ba1829c15d7bc8004b98cde5d277
yardobr <yar.dobroskok@gmail.com>
2022-04-10 17:36:52 +0300

    Updated frame responsive styles amd template

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,9 +3,17 @@
     if (isClosed) return;
 
     var node = document.createElement('iframe');
+    node.id = 'helpua-frame'
     node.src = 'http://volunteer.terapiya.space/assets/footer.html';
     node.width = '100%';
     node.height = '48px';
-    node.style = 'position:fixed; bottom: 0; height: 48px; border: none;z-index: 10000;'
+    node.style = `position: fixed;
+                  bottom: 0;
+                  height: 48px;
+                  border: none;
+                  z-index: 10000;
+                  @media screen and (max-width: 742px) {
+                      #helpua-frame { height: 92px; }
+                  }`
     document.body.appendChild(node);
 })();
