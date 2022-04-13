60f3f101eb52ff5d28b6594bb6fbc4857bf37b0d
yardobr <yar.dobroskok@gmail.com>
2022-04-13 14:32:52 +0300

    Wait for document loaded

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,36 @@
-(function() {
-    var isClosed = localStorage.getItem('helpUA-closed');
-    if (isClosed) return;
-
-    var matchesMedia = window.matchMedia('(max-width: 742px)');
-
-    var frame = document.createElement('iframe');
-    frame.id = 'helpua-frame'
-    frame.src = 'http://volunteer.terapiya.space/assets/footer.html';
-    frame.width = '100%';
-    frame.style = `
-        position: fixed;
-        bottom: 0;
-        border: none;
-        z-index: 10000;
-    `;
-
-    var placeholder = document.createElement('div');
-    placeholder.id = 'helpua-placeholder';
-    placeholder.style.height = getHeight(matchesMedia);
-
-    recalculateStyles(frame, placeholder);
-
-    window.onresize = function() { recalculateStyles(frame, placeholder) };
-
-    document.body.appendChild(frame);
-    document.body.appendChild(placeholder);
-})();
+var stateCheck = setInterval(() => {
+    if (document.readyState === 'complete') {
+      clearInterval(stateCheck);
+      (function() {
+        var isClosed = localStorage.getItem('helpUA-closed');
+        if (isClosed) return;
+    
+        var matchesMedia = window.matchMedia('(max-width: 742px)');
+    
+        var frame = document.createElement('iframe');
+        frame.id = 'helpua-frame'
+        frame.src = 'http://volunteer.terapiya.space/assets/footer.html';
+        frame.width = '100%';
+        frame.style = `
+            position: fixed;
+            bottom: 0;
+            border: none;
+            z-index: 10000;
+        `;
+    
+        var placeholder = document.createElement('div');
+        placeholder.id = 'helpua-placeholder';
+        placeholder.style.height = getHeight(matchesMedia);
+    
+        recalculateStyles(frame, placeholder);
+    
+        window.onresize = function() { recalculateStyles(frame, placeholder) };
+    
+        document.body.appendChild(frame);
+        document.body.appendChild(placeholder);
+        })();
+    }
+  }, 100);
 
 function getHeight (matchesMedia) {
     return matchesMedia.matches ? '92px' : '48px';
